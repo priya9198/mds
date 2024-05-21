2472b59243da435fb40228c836dce2a60b84c955
priya9198 <priya016978@gmail.com>
2024-05-21 15:53:17 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,17 @@
 import streamlit as st
 import pickle
 import pandas as pd
+
 # Title of the app
 st.write('# Diabetes Prediction App')
+
+# Load your trained model
+try:
+    model = pickle.load(open('model.pkl', 'rb'))
+except FileNotFoundError:
+    st.error("Model file not found. Please make sure 'model.pkl' is in the current directory.")
+    st.stop()
+
 # Input fields for the features
 st.sidebar.header('User Input Parameters')
 
