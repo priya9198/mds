5d5218e1615e439c2b6f6ab7196a2148120d7b1a
priya9198 <priya016978@gmail.com>
2024-05-21 16:00:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,3 @@
 import streamlit as st
-st.write('# Streamlit calculator')
-number1= st.number_input('number 1')
-number2 = st.number_input('number 2')
-num3 = number1+number2
-st.write('# Answer is ',num3)
+import pickle
+import pandas as pd
